c3ffaf049eadae7a16065d2dac8dba5e89e88661
Wilson Young <yyfearth@gmail.com>
2013-04-28 12:32:50 -0700

    Embedded Address model into User model

--- service/app.rb
+++ service/app.rb
@@ -27,7 +27,6 @@ class App < Sinatra::Application
     MongoMapper.connection.connect
   end
 
-  require 'models/address'
   require 'models/user'
   require 'controllers/base'
   require 'controllers/user'
--- service/models/user.rb
+++ service/models/user.rb
@@ -2,6 +2,26 @@ class User
   include MongoMapper::Document
   safe
 
+  class Address
+    include MongoMapper::EmbeddedDocument
+
+    key :address, String
+    key :city, String
+    key :state, String
+    key :zip, String
+    key :country, String
+
+    def empty?
+      self.address.to_s.empty? and self.city.to_s.empty? and
+          self.state.to_s.empty? and self.zip.to_s.empty? and self.country.to_s.empty?
+    end
+
+    def serializable_hash(options = {})
+      super({except: :id}.merge(options))
+    end
+
+  end
+
   key :email, String, :required => true
   key :password, String, :required => true
   key :first_name, String, :required => true
@@ -24,3 +44,4 @@ class User
 end
 
 User.ensure_index :email, :unique => true
+
