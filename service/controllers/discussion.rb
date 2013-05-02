405758ec95bad00813df168de7b7f828c2b14e94
Mason Wan <masonwan@gmail.com>
2013-05-02 14:05:42 -0700

    Initialize discussion controller and model

--- service/controllers/discussion.rb
+++ service/controllers/discussion.rb
@@ -10,4 +10,27 @@ class DiscussionController < EntityController
   get GET_BY_COURSE_URL do
     'GET_BY_COURSE_URL'
   end
+
+  post '/discussion' do
+    request_content = request.body.read
+
+    begin
+      json = JSON.parse(request_content)
+      discussion = Discussion.create
+      created discussion.to_json
+    rescue MongoMapper::DocumentNotValid => e
+      invalid_entity! e
+    rescue JSON::ParserError => e
+      err 400, 'BAD_REQUEST', 'Cannot parse the request data' if request_content
+    end
+  end
+
+  # HACK: for debug
+  get '/discussions/all' do
+    ok Discussion.all
+  end
+
+  get '/messages/all' do
+    ok Message.all
+  end
 end
--- service/models/discussion.rb
+++ service/models/discussion.rb
@@ -1,6 +1,37 @@
+require 'time'
+
 class Discussion
   include MongoMapper::Document
-  safe
 
+  many :Message, {in: :message_ids}
 
+  key :course_id, String
+  key :title, String, {require: true}
+  key :created_by, String, {require: true}
+  key :created_at, Time, {require: true, default: Time.now}
+  key :update_at, Time, {require: true, default: Time.now}
+  key :message_ids, Array
+  timestamps!
+
+  attr_protected :created_at
+
+  def to_s
+    "#{title} #{message_ids.size}"
+  end
+end
+
+class Message
+  include MongoMapper::Document
+
+  belongs_to :Discussion
+
+  key :title, String, {require: true}
+  key :content, String, {require: true}
+  key :discussion_id, String, {require: true}
+  key :created_by, String, {require: true}
+  key :created_at, Time, {require: true, default: Time.now}
+  key :update_at, Time, {require: true, default: Time.now}
+  timestamps!
+
+  attr_protected :created_at
 end
