637676f8e7faa997713455626c9cec785dfcfadb
Mason Wan <masonwan@gmail.com>
2013-05-03 13:43:01 -0700

    Implement all APIs

--- service/controllers/discussion.rb
+++ service/controllers/discussion.rb
@@ -1,42 +1,21 @@
+require '../models/discussion'
+
 class DiscussionController < EntityController
   SINGLE_ID_URL = '/discussion/:id'
-  GET_BY_COURSE_URL = '/discussion/course/:id'
+  SINGLE_URL = '/discussion'
+  MULTIPLE_URL = '/discussions'
+  GET_BY_COURSE_URLS = %w(/discussion/course/:id /course/:id/discussion)
   ADD_DISCUSSION_URL = '/discussion'
 
-  get SINGLE_ID_URL do
-    p params
-    err 400, 'BAD_REQUEST', 'No id' if params[:id].nil?
+  @entity = Discussion.name
+  @entity_name = Discussion.name
 
-    p 'yes'
-  end
-
-  get GET_BY_COURSE_URL do
-    # TODO
-    'GET_BY_COURSE_URL'
-  end
-
-  get '/discussions/' do
-    # TODO
-    'search api'
-  end
-
-  post '/discussion' do
-    request_content = request.body.read
-
+  # Store the request JSON globally.
+  before '*' do
     begin
-      json = JSON.parse(request_content)
-
-      err 400, 'BAD_REQUEST', "The request data doesn't match expected format" unless %w(title created_by).all? { |k| json.has_key?(k) }
-
-      discussion = Discussion.create(
-          {
-              title: json['title'],
-              created_by: json['created_by'],
-          }
-      )
-      created discussion
-    rescue MongoMapper::DocumentNotValid => e
-      invalid_entity! e
+      request_content = request.body.read
+      puts request_content.empty? ? 'No content' : 'Request content = ' << request_content
+      @json = JSON.parse(request_content) unless request_content.to_s.empty?
     rescue JSON::ParserError => e
       warn e.backtrace[0]
       warn e.inspect
@@ -44,47 +23,70 @@ class DiscussionController < EntityController
     end
   end
 
+  # Store the id globally.
+  [GET_BY_COURSE_URLS, SINGLE_ID_URL].each do |url|
+    before url do
+      @id = params[:id]
+      puts 'id = ' << @id.to_s
+    end
+  end
+
+  get SINGLE_ID_URL do
+    discussion = Discussion.find_by_id(@id)
+
+    if discussion.nil?
+      id_not_found(Discussion, @id)
+    else
+      ok(discussion)
+    end
+  end
+
+  GET_BY_COURSE_URLS.each do |path|
+    get path do
+      discussion = Discussion.first({course_id: @id})
+
+      id_not_found(Course, @id) if discussion.nil?
+
+      ok(discussion)
+    end
+  end
+
+  get MULTIPLE_URL do
+    ok(do_search(Discussion, params, {q: [:title], fields: [:course_id], }))
+    puts 'Search discussion: ' << discussion.inspect
+  end
+
+  post SINGLE_URL do
+    bad_request('The request data does not match expected format') unless %w(title created_by).all? { |k| @json.has_key?(k) }
+    discussion = Discussion.create(@json)
+    puts 'Create discussion: ' << discussion.inspect
+    created(discussion)
+  end
+
   put SINGLE_ID_URL do
-    # TODO
-    'update discussion'
+    discussion = Discussion.find_by_id(@id)
+
+    id_not_found(Discussion, @id) if discussion.nil?
+
+    @json.each { |k| discussion[k] = @json[k] }
+
+    discussion.save
+
+    puts 'Update discussion: ' << discussion.inspect
+
+    ok(discussion)
   end
 
   delete SINGLE_ID_URL do
-    # TODO
-    'delete discussion'
-  end
+    discussion = Discussion.find(@id)
+    not_found_if_nil(discussion)
 
-  get '/discussion/:id/messages' do
-    # TODO
-    'search messages'
-  end
-
-  get '/discussion/:id/message/:message_id' do
-    # TODO
-    'get a messages'
-  end
-
-  post '/discussion/:id/message' do
-    # TODO
-    'create a message'
-  end
-
-  put '/discussion/:id/message/:message_id' do
-    # TODO
-    'update a message'
-  end
-
-  delete '/discussion/:id/message/:message_id' do
-    # TODO
-    'delete a message'
+    discussion.destroy
+    ok('Delete discussion ' << @id)
   end
 
   # HACK: for debug
   get '/discussions/all' do
-    ok Discussion.all
-  end
-
-  get '/messages/all' do
-    ok Message.all
+    ok(Discussion.all)
   end
 end
