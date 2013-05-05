c3e89eb7e0168aaad24ac2ce0b3cd1d2d2d9fb78
Mason Wan <masonwan@gmail.com>
2013-05-05 08:37:55 -0700

    Backup working progress

--- service/spec/controllers/user_spec.rb
+++ service/spec/controllers/user_spec.rb
@@ -1,7 +1,37 @@
 require_relative '../spec_helper'
 
+# TODO
 describe 'UserController' do
-  it 'should return json' do
+  include SpecCommon
+
+  it 'creats a user' do
+    request_data = {
+        email: 'test@test.com',
+        password: '',
+        title: 'Test Discussion',
+    }
+
+    post '/discussion', request_data.to_json
+    last_response.status.should == 201
+
+    @vars[:discussion] = response_data = JSON.parse last_response.body
+
+    (/[\w\d]/ =~ response_data['id']).nil?.should be_false
+  end
+
+  it 'gets a user' do
+    get '/users'
+    puts last_response.body
+    last_response.should be_ok
+  end
+
+  it 'updates a user' do
+    get '/users'
+    puts last_response.body
+    last_response.should be_ok
+  end
+
+  it 'deletes a user' do
     get '/users'
     puts last_response.body
     last_response.should be_ok
