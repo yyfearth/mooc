d4432ea70d32082288bfadea80a930600177ec6d
Mason Wan <masonwan@gmail.com>
2013-05-05 07:36:47 -0700

    * Fix update controller * Remove the parentheses

--- service/controllers/discussion.rb
+++ service/controllers/discussion.rb
@@ -16,7 +16,7 @@ DISCUSSION_COURSE_URLS.concat([DISCUSSION_ID_URL]).each do |url|
 end
 
 get DISCUSSION_ID_URL do
-  discussion = Discussion.find_by_id(@id)
+  discussion = Discussion.find_by_id @id
 
   not_found_if_nil!(discussion)
 
@@ -27,7 +27,7 @@ DISCUSSION_COURSE_URLS.each do |path|
   get path do
     discussion = Discussion.first({course_id: @id})
 
-    not_found_if_nil!(discussion)
+    not_found_if_nil! discussion
 
     ok(discussion)
   end
@@ -39,34 +39,36 @@ get DISCUSSIONS_URL do
 end
 
 post DISCUSSION_URL do
-  discussion = Discussion.create(@json)
+  discussion = Discussion.create @json
   puts 'Create discussion: ' << discussion.inspect
   created(discussion)
 end
 
 put DISCUSSION_ID_URL do
-  discussion = Discussion.find_by_id(@id)
+  discussion = Discussion.find_by_id @id
 
-  not_found_if_nil!(discussion)
+  not_found_if_nil! discussion
+  bad_request! if @json.has_key? :id || discussion[:id] != @json[:id]
 
-  @json.each { |k| discussion[k] = @json[k] }
-
-  discussion.save
+  allowed_fields = %w(course_id title created_by)
+  allowed_fields.each do |field|
+    discussion[field] = @json[field] unless @json[field].nil?
+  end
 
   puts 'Update discussion: ' << discussion.inspect
 
-  ok(discussion)
+  ok discussion
 end
 
 delete DISCUSSION_ID_URL do
   discussion = Discussion.find(@id)
-  not_found_if_nil!(discussion)
+  not_found_if_nil! discussion
 
   discussion.destroy
-  ok('Delete discussion ' << @id)
+  ok JSON.parse discussion
 end
 
 # HACK: for debug
 get '/discussions/all' do
-  ok(Discussion.all)
+  ok Discussion.all
 end
