a91a28c4ecff86287b303b0619dcf5850ffc2985
Mason Wan <masonwan@gmail.com>
2013-05-05 15:55:33 -0700

    All CRUD test cases

--- service/spec/controllers/course_spec.rb
+++ service/spec/controllers/course_spec.rb
@@ -8,45 +8,68 @@ describe 'Course API' do
 
   it 'creates a course' do
     request_data = {
-        name: ''
+        title: random_text(16),
+        description: 'The New Course',
+        attachments: [],
+        category_id: random_id,
+        participants: [
+            {
+                email: 'instructer@test.com',
+                role: 'instructer',
+                status: 'drunk',
+            },
+            {
+                email: 'student@test.com',
+                role: 'student',
+                status: 'cooked',
+            },
+        ],
+        created_by: 'instructer@test.com',
+        updated_by: 'instructer@test.com',
     }
 
     post '/course', request_data.to_json
+    @vars[:course] = course = JSON.parse(last_response.body)
+    puts course['message'] unless course['error'].nil?
+
     last_response.status.should == 201
     last_response.body.nil?.should be_false
-    @vars[:course] = JSON.parse(last_response.body)
+    course['id'].nil?.should be_false
   end
 
   it 'gets a course' do
-    get '/course/' << @vars[:course]['email']
+    get '/course/' << @vars[:course]['id']
+
     last_response.status.should == 200
   end
 
   it 'updates a course' do
     request_data = {
-        #email: ((0..6).map { (65 + rand(26)).chr }.join) << '@test.com',
-        password: '0000000000000000000000000000000000000000',
-        first_name: '~James',
-        last_name: '~Smith',
-        address: {
-            address: '~760 United Nations Plaza',
-            city: '~New York',
-            state: '~NY',
-            zip: '~10017',
-            country: '~USA',
-        },
+        id: @vars[:course]['id'],
+        title: random_text(16),
+        description: '~update Course',
+        attachments: [],
+        category_id: '18694174d09cd1bc80000260',
+        participants: [],
+        created_by: 'instructer2@test.com',
+        updated_by: 'instructer2@test.com',
     }
 
-    put '/course/' << @vars[:course]['email'], request_data.to_json
+    put '/course/' << request_data[:id], request_data.to_json
+    @vars[:course] = course = JSON.parse(last_response.body)
+    puts course['message'] unless course['error'].nil?
+
     last_response.status.should == 200
     last_response.body.nil?.should be_false
 
-    @vars[:course] = course = JSON.parse(last_response.body)
-    course['password'].should == request_data[:password]
+    course['name'].should == request_data[:name]
   end
 
   it 'deletes a course' do
-    delete '/course/' << @vars[:course]['email']
+    delete '/course/' << @vars[:course]['id']
+    @vars[:course] = course = JSON.parse(last_response.body)
+    puts course['message'] unless course['error'].nil?
+
     last_response.status.should == 200
   end
 end
