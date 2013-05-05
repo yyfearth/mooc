6b4fe2068148c80d89ef0c1a998fbe98e8bea421
Mason Wan <masonwan@gmail.com>
2013-05-05 15:52:45 -0700

    Add few random text generators

--- service/spec/spec_helper.rb
+++ service/spec/spec_helper.rb
@@ -9,3 +9,25 @@ end
 RSpec.configure do |config|
   config.include Rack::Test::Methods
 end
+
+LOWER_ALPHABETS = ('a'..'z').to_a
+UPPER_ALPHABETS = ('A'..'Z').to_a
+ALPHABETS = LOWER_ALPHABETS + UPPER_ALPHABETS
+NUMBERS = ('0'..'9').to_a
+CHARACTERS = ALPHABETS + NUMBERS
+
+def random_text(length)
+  (0...length).map { (ALPHABETS + [' '])[rand(ALPHABETS.size + 1)] }.join
+end
+
+def random_hex(length)
+  (0...length).map { ('a'..'z').to_a[rand(26)] }.join
+end
+
+def random_id
+  random_hex(24)
+end
+
+def random_email
+  random_text(8) << '@test.com'
+end
