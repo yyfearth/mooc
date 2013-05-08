5d00a3a352a5a6283cc62d155642eeca55e56376
Wilson Young <yyfearth@gmail.com>
2013-05-08 15:04:49 -0700

    Use Flat-UI and change erb to slim

--- moocui/app/controllers/home_controller.rb
+++ moocui/app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @title = "Welcome to Project MOOC"
     respond_to do |format|
-      format.html # index.html.erb
+      format.html # index.html.slim
       format.json { render :json => @title }
     end
   end
