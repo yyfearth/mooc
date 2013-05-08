eab256df4b99fe57491330c98e5038deceb75612
Mason Wan <masonwan@gmail.com>
2013-05-08 12:52:18 -0700

    Fix dependency name

--- service/models/message.rb
+++ service/models/message.rb
@@ -1,11 +1,11 @@
 class Message
   include MongoMapper::Document
 
-  belongs_to :Discussion
+  belongs_to :discussion
 
   key :title, String, {require: true}
   key :content, String, {require: true}
-  key :discussion_id, String, {require: true}
+  #key :discussion_id, String, {require: true}
   key :created_by, String, {require: true}
   timestamps!
 
